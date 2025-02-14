de8e6b77474833b4ff067f445cbb75bb4c7ab3ce
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-14 15:17:26 +0100

    start

---
+++ csript.js
@@ -0,0 +1,8 @@
+let xp = 0; // Erfahrungspunkte (XP) starten bei 0
+let health = 100; //Der Spieler startet mit 100 Lebenspunkten
+let gold = 50; // Der Spieler beginnt mit 50 Goldstücken
+let currentWeaponIndex = 0; // Index der aktuellen Waffe (Startwaffe)
+let fighting; // Speichert, ob der Spieler im Kampf ist (noch nicht initialisiert)
+let monsterHealth; // Speichert die Lebenspunkte des Monsters (noch nicht gesetzt)
+let inventory = ["stick"]; // Das Inventar startet mit einem Stock als Waffe
+
